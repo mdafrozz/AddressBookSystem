1eb726a16f3daf096b9a14efd108730da20dfdd5
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-07-01 09:21:20 +0530

    Refactor: Ability to add multiple person to Address Book

--- AddressBook.java
+++ AddressBook.java
@@ -19,10 +19,51 @@ public class AddressBook {
 	static Scanner scanner = new Scanner(System.in);
 
 	// Create Contacts
+	public void createContact() {
+		System.out.println("Enter Person Details:");
+		System.out.println("Enter first Name");
+		String firstName = scanner.next();
+
+		System.out.println("Enter last Name");
+		String lastName = scanner.next();
+
+		System.out.println("Enter Email");
+		String email = scanner.next();
+
+		System.out.println("Enter phone number");
+		String phoneNumber = scanner.next();
+
+		System.out.println("Enter address");
+		String address = scanner.next();
+
+		System.out.println("Enter state");
+		String state = scanner.next();
+
+		System.out.println("Enter city");
+		String city = scanner.next();
+
+		System.out.println("Enter zip");
+		String zip = scanner.next();
+
+		listContact.add(new Contact(firstName, lastName, email, phoneNumber, address, state, city, zip));
+		// printContacts();
+	}
 	// Adding new contact method
 	public void addContact(Contact contact) {
 		
-		
+		try {
+			createContact();
+			System.out.println("Enter firstname and lastname");
+			String name = scanner.next() + "" + scanner.next();
+
+			Iterator<Contact> iterator = listContact.listIterator();
+
+			while (iterator.hasNext()) {
+				Contact contacts = iterator.next();
+				if (name.equals(contacts.getFirstName() + "" + contacts.getLastName())) {
+					System.out.println("Duplicate entry add new contact press 1 ");
+				} else {
+					
 				System.out.println("Enter first Name");
 					String firstName = scanner.next();
 
@@ -50,7 +91,11 @@ public class AddressBook {
 					listContact.add(new Contact(firstName, lastName, email, phoneNumber, address, state, city, zip));
 					printContacts();
 		}
-	
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 	// Print Contacts
 		public Contact printContacts() {
 			if (listContact.isEmpty()) {
